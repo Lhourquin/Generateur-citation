203615ace692f2283e966d0596939c2b55352796
Lhourquin <lhourquin@gmail.com>
2019-05-01 15:44:11 +0200

    remove line of code import compoenents for HelloWorld.vue, and on app template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CitationHamlet/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CitationHamlet from './components/CitationHamlet.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CitationHamlet
   }
 }
 </script>
