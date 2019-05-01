1d4fae79c1a78222a5f55b8e4699a5e455e6e1e2
Lhourquin <lhourquin@gmail.com>
2019-05-02 01:34:11 +0200

    remove citation directory, and insert props on Citation.vue, and h1 and h2 on template

--- src/components/Citation.vue
+++ src/components/Citation.vue
@@ -1,41 +1,15 @@
 <template>
 <div>
-    <citation-albert/>
-    <citation-confucius/>
-    <citation-hamlet/>
-    <citation-jean-paul/>
-    <citation-raymond/>
+    <h1>{{component.citation}}</h1>
+    <h2>{{component.authors}}</h2>
 </div>
     
 </template>
 
 <script>
-import CitationHamlet from './citation/CitationHamlet.vue'
-import CitationConfucius from './citation/CitationConfucius.vue'
-import CitationJeanPaul from './citation/CitationJeanPaul.vue'
-import CitationAlbert from './citation/CitationAlbert.vue'
-import CitationRaymond from './citation/CitationRaymond.vue'
-
 export default {
     name : 'Citation',
-    data () {
-    return {
-      componentNames : [
-        'CitationHamlet',
-        'CitationConfucius',
-        'CitationJeanPaul',
-        'CitationAlbert',
-        'CitationRaymond'
-      ]
-    }
-  },
-  components: {
-    CitationHamlet,
-    CitationConfucius,
-    CitationJeanPaul,
-    CitationAlbert,
-    CitationRaymond
-  }
+    props : ['component']
 }
 </script>
 
