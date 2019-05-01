d25a8d678e9979334daaa2a7ee03ec92126458da
Lhourquin <lhourquin@gmail.com>
2019-05-01 17:12:00 +0200

    create citation.vue dans citation directory with all ciatation componant and add imprt for this in citation.vue

---
+++ src/components/Citation.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <citation-albert/>
+    <citation-confucius/>
+    <citation-hamlet/>
+    <citation-jean-paul/>
+    <citation-raymond/>
+</div>
+    
+</template>
+
+<script>
+import CitationHamlet from './citation/CitationHamlet.vue'
+import CitationConfucius from './citation/CitationConfucius.vue'
+import CitationJeanPaul from './citation/CitationJeanPaul.vue'
+import CitationAlbert from './citation/CitationAlbert.vue'
+import CitationRaymond from './citation/CitationRaymond.vue'
+
+export default {
+    name : 'Citation',
+    data () {
+    return {
+      componentNames : [
+        'CitationHamlet',
+        'CitationConfucius',
+        'CitationJeanPaul',
+        'CitationAlbert',
+        'CitationRaymond'
+      ]
+    }
+  },
+  components: {
+    CitationHamlet,
+    CitationConfucius,
+    CitationJeanPaul,
+    CitationAlbert,
+    CitationRaymond
+  }
+}
+</script>
+
+<style>
+
+</style>
