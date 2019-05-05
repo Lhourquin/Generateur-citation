636e0d40390bc6c9c1872d5cf5a57b8b4808a26b
Lhourquin <lhourquin@gmail.com>
2019-05-05 17:24:16 +0200

    insert data and methods with event for random quotation

--- src/components/Citation.vue
+++ src/components/Citation.vue
@@ -1,7 +1,13 @@
 <template>
 <div>
-    <h1>{{component.citation}}</h1>
-    <h2>{{component.authors}}</h2>
+    <div 
+    v-for="component in componentDisplay" 
+    :key="component.id">
+    <h1>{{component.id}}</h1>
+    </div>
+    <button
+    v-on:click="eventRandom"
+    >Random</button>
 </div>
     
 </template>
@@ -9,8 +15,49 @@
 <script>
 export default {
     name : 'Citation',
-    props : ['component']
+    data () {
+    return {
+      componentNames : [
+        {citation : 'On se prend souvent pour quelqu\'un, alors qu\'au fond, on est plusieurs.',
+        authors : 'Raymond Devos'},
+
+        {citation : 'Exige beaucoup de toi-même et attends peu des autres. Ainsi beaucoup d\'ennuis te seront épargnés.',
+        authors : 'Confucius'},
+
+        {citation : 'La vie, c\'est comme une bicyclette, il faut avancer pour ne pas perdre l\'équilibre.',
+        authors : 'Albert Einstein'},
+
+        {citation : 'Qu\'est ce que l\'homme, si le bien suprême, l\'aubaine de sa vie est uniquement de dormir et de manger ? ... Une bête, rien de plus.',
+        authors : 'William Shakespeare'},
+
+        {citation : 'Si quelqu\'un t\'a offensé, ne cherche pas à te venger. Assieds-toi au bord de la rivière et bientôt tu verras passer son cadavre.',
+        authors : 'Lao Tseu'}
+      ],
+      index : null,
+      componentDisplay : []
+    }
+},
+    methods : {
+        getRandom : function () {
+            this.index = this.componentNames[Math.floor(Math.random() * this.componentNames.length)]
+        },
+        pushRandom : function () {
+            this.componentDisplay.push({
+                id :this.index
+            })
+        },
+        popRandom : function () {
+            if(this.componentDisplay.length > 1)
+            this.componentDisplay.shift()
+        },
+        eventRandom : function () {
+            this.getRandom()
+            this.pushRandom()
+            this.popRandom()
+        }
+    }
 }
+
 </script>
 
 <style>
